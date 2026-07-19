8168d1266049bd89b9461269e0a1f2dc5b545f10
Justin Nel <justin@cyber-lane.com>
2026-07-19 19:33:32 +0900

    fix: follow Homebrew formula conventions

--- Formula/syncpick.rb
+++ Formula/syncpick.rb
@@ -2,17 +2,16 @@ class Syncpick < Formula
   desc "Interactive fzf picker for syncing selected SSH folders with rsync"
   homepage "https://github.com/Cyberlane/syncpick"
   url "https://github.com/Cyberlane/syncpick/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
   sha256 "5380fb2e87fae04952493fd080fea77f8b1df5b92d4bf29320e1e9ac12698080"
   license "MIT"
-  revision 2
+  revision 3
 
   depends_on "fzf"
   depends_on "rsync"
   depends_on "zsh"
 
   def install
-    inreplace "syncpick", "#!/bin/zsh", "#!#{Formula["zsh"].opt_bin}/zsh"
+    inreplace "syncpick", "#!/bin/zsh", "#!#{formula_opt_bin("zsh")}/zsh"
     bin.install "syncpick"
     bin.install_symlink bin/"syncpick" => "sp"
   end
