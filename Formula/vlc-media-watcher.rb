9cb8252671f6ff6a6ee876450d3110a69b275e57
Justin Nel <justin@cyber-lane.com>
2026-08-15 23:59:31 +0900

    build: release vlc-media-watcher 0.2.1

--- Formula/vlc-media-watcher.rb
+++ Formula/vlc-media-watcher.rb
@@ -1,16 +1,16 @@
 class VlcMediaWatcher < Formula
   desc "Local, safe-by-default VLC watch tracking with a terminal UI"
   homepage "https://github.com/Cyberlane/vlc-media-watcher"
-  url "https://github.com/Cyberlane/vlc-media-watcher/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "0d1d889f60ecd1c2c3bd44eef39615116c8e1337fc688ae79dcb492822e3bbde"
+  url "https://github.com/Cyberlane/vlc-media-watcher/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "bdf0d972816c56b85a17e74132c5d81803b9403af6347e71742f01f06bc297ea"
   license "MIT"
 
   depends_on "go" => :build
 
   def install
     ldflags = %w[
-      -X main.version=0.2.0
-      -X main.commit=v0.2.0
+      -X main.version=0.2.1
+      -X main.commit=v0.2.1
       -X main.date=unknown
       -X main.builtBy=homebrew
     ]
@@ -26,6 +26,6 @@ class VlcMediaWatcher < Formula
   end
 
   test do
-    assert_match "vlc-media-watcher 0.2.0", shell_output("#{bin}/vlc-media-watcher version")
+    assert_match "vlc-media-watcher 0.2.1", shell_output("#{bin}/vlc-media-watcher version")
   end
 end
